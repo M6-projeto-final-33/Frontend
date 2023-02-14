3a6c3898adb5aff6149e55d90dbd397cb6d71f43
Laís Martins <silveiramartinslais@gmail.com>
2023-02-14 12:19:00 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Motors Shop</title>
+        <meta
+          name="description"
+          content="encontre seu veículo dos sonhos aqui"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Motors Shop</h1>
+    </div>
+  );
+};
+
+export default Home;
