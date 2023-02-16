20cf34e8c587c5ea67ff4b55937dce5aceb76b8b
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-02-16 14:02:02 -0400

    fix: default home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,6 @@ import ProductCard from "../components/ProductCard";
 import ProductCardAuction from "../components/ProductCardAuction";
 
 const Home: NextPage = () => {
-
-  const product = {
-    title: "product",
-    description: "description",
-    imageUrl: "https://media.istockphoto.com/id/1133010686/pt/foto/toyota-corolla-sedan-in-hybrid-version-on-the-street.jpg?s=1024x1024&w=is&k=20&c=bXKpnBULet_xiF-c2OwdiPDnRjDMliUsJJNC9U6vYbc=",
-    advertiser: "Antonio",
-    advertiserUrl: "https://media.istockphoto.com/id/1133010686/pt/foto/toyota-corolla-sedan-in-hybrid-version-on-the-street.jpg?s=1024x1024&w=is&k=20&c=bXKpnBULet_xiF-c2OwdiPDnRjDMliUsJJNC9U6vYbc=",
-    kilometers: "10",
-    year: "2039",
-    value: "88.000,00"
-  }
-
   return (
     <div>
       <Head>
@@ -29,8 +17,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Motors Shop</h1>
-      <ProductCardAuction product={product} />
-      <ProductCard product={product} />
     </div>
   );
 };
