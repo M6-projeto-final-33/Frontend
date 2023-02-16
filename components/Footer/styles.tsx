fe7672c0b0623fa12cc25e7ceeb5c5c3e9f31f7f
byetevinn <stevan14padilha@gmail.com>
2023-02-15 22:18:53 -0300

    Fix: footer responsiveness

--- components/Footer/styles.tsx
+++ components/Footer/styles.tsx
@@ -2,14 +2,17 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
 
-  height: 140px;
+  height: calc(310px - 20vw);
+  min-height: 140px;
+  min-width: max-content;
 
   background-color: var(--grey0);
 
-  padding: 0px 60px;
+  padding: 0px 30px;
 
   strong {
     display: flex;
@@ -22,5 +25,9 @@ export const StyledFooter = styled.footer`
   Button {
     width: 53px;
     height: 50px;
+  }
+
+  @media (min-width: 720px) {
+    flex-direction: row;
   }
 `;
