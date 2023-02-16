c45a1461cc69951e14ec44b22d3f74a1f00a1f81
byetevinn <stevan14padilha@gmail.com>
2023-02-16 14:48:15 -0300

    Feat: Initial data of the modal

---
+++ components/Modal/index.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+
+import Button from "../Button";
+
+import { StyledModal } from "./style";
+
+interface IModal {
+  children: ReactNode;
+  titleModal: string;
+  isDoubleButton: boolean;
+  contentButtonOne: string;
+  contentButtonTwo: string;
+  setModalIsOpen: (data: boolean) => void;
+}
+
+const Modal = ({
+  children,
+  titleModal,
+  isDoubleButton,
+  contentButtonOne,
+  contentButtonTwo,
+  setModalIsOpen,
+}: IModal) => {
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <StyledModal>
+      <div className="div_header">
+        <h1>{titleModal}</h1>
+
+        <button onClick={closeModal}>X</button>
+      </div>
+
+      {children}
+
+      <div className="div_buttons">
+        {isDoubleButton && (
+          <Button colorStyle="grey-6__grey-2">{contentButtonTwo}</Button>
+        )}
+        <Button colorStyle="brand-1__white-fixed"> {contentButtonOne} </Button>
+      </div>
+    </StyledModal>
+  );
+};
+
+export default Modal;
