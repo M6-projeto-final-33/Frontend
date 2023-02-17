2861ee4f96f2d9cffde340ce462aff212987a143
Laís Martins <silveiramartinslais@gmail.com>
2023-02-17 13:13:09 -0300

    feat: context provider configured in __app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from "next/app";
 import GlobalStyles from "../styles/GlobalStyles";
+import Provider from "../contexts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
