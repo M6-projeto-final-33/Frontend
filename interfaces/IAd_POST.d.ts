b8a116bb6522acdfea1531b286adcca255c21c0a
Laís Martins <silveiramartinslais@gmail.com>
2023-02-23 12:47:52 -0300

    feat: Ad interfaces created

---
+++ interfaces/IAd_POST.d.ts
@@ -0,0 +1,11 @@
+export interface IAd_POST {
+  typeAd: string;
+  title: string;
+  year: string;
+  mileage: string;
+  price: number;
+  description: string;
+  vehicleType: string;
+  coverImg: string;
+  userId: string;
+}
